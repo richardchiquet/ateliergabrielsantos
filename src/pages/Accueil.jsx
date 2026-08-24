976d51126b5c1bd24888468cd7ba558a971a3824
richardchiquet <56953330+richardchiquet@users.noreply.github.com>
2026-08-24 20:34:52 +0200

    Update Accueil.jsx

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 // Swiper
 import "swiper/css";
+import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation } from "swiper/modules";
 
